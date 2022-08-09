68c3739dc9f1e8167a34c919bb464686fad2707c
Cassio Luis Xavier da S.Fabiano <cassio.fabiano@trescon.com.br>
2022-08-08 22:38:35 -0300

    Validadno formulario com Formik

--- src/components/AdicionaCliente.jsx
+++ src/components/AdicionaCliente.jsx
@@ -1,25 +1,72 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, useField } from 'formik';
+
+const Campo = ({label, ...props}) => {
+  const [field, meta] = useField(props)
+
+  return (
+    <div className='form-group'>
+      <label htmlFor={props.id}>{label}</label>
+      <input 
+        {...field}
+        {...props}
+        className={ meta.error && meta.touched ? 'isError' : ''}
+      />
+      {meta.error && meta.touched ? (<div className='error-mensage'>{meta.error}</div>) : null}
+    </div>
+  )
+}
+
 
 const AdicionaCliente = () => {
+
+  const handleSubmit = values => {
+    alert(JSON.stringify(values))
+  }
+
+  const handleValidade = values => {
+    const erros = {}
+    if(!values.nome)
+      erros.nome = 'O nome é um campo obrigatório'
+    if(!values.email){
+      erros.email = 'O email é um campo obrigatório'
+    } else if(!/^[a-z0-9.]+@[a-z0-9]+\.[a-z]{2,}$/i.test(values.email)) {
+      erros.email = 'O email invalido'
+    }
+    if(!values.nascimento)
+      erros.nascimento = 'A data de nascimento é obrigatório'
+
+    return erros;
+  }  
+
   return (
     <> 
       <h1>Cadastro de Clientes</h1>
-      <Formik onSubmit={values => alert(JSON.stringify(values))} initialValues={{ nome: '', email: '', nascimento: ''}}>
+      <Formik 
+      onSubmit={(val) => handleSubmit(val)} 
+      validate={(val) => handleValidade(val)}
+      initialValues={{ nome: '', email: '', nascimento: ''}}
+      >
         {props => (
         <form onSubmit={props.handleSubmit} noValidate>
-          <div className="form-group">
-            <label htmlFor="nome">Nome</label>
-            <input id="nome" name="nome" type="text" onChange={props.handleChange}/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input id="email" name="email" type="email" onChange={props.handleChange}/>
-          </div>
-          <div className="form-group">
-            <label htmlFor="date">Data de Nascimento</label>
-            <input id="nascimento" name="nascimento" type="date" onChange={props.handleChange}/>
-          </div>
+            <Campo 
+            id="nome" 
+            name="nome" 
+            type="text" 
+            label={"Nome"}
+            />
+            <Campo 
+            id="email" 
+            name="email" 
+            type="email" 
+            label={"Email"}
+            />
+            <Campo 
+            id="nascimento" 
+            name="nascimento" 
+            type="date"
+            label={"Data de Nascimento"} 
+            />
           <button type="submit">Adicionar</button>
         </form>
         )}
